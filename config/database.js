892494b07ae263a423bcda9330a80d734daff8a4
frasasa08-creator <fra.sasa08@gmail.com>
2026-05-27 20:39:46 +0200

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,12 +1,9 @@
-// database.js - Compatibilità con il vecchio codice che importa database.js
-// Usa il db SQLite locale tramite db.js
-const db = require('./db');
+const db = require('../db');
 
 function createDatabase() {
     return db;
 }
 
-// dbConfig vuoto per compatibilità (non più necessario)
 const dbConfig = {};
 
 module.exports = { createDatabase, dbConfig };
